2c7e2ab5110764a5afa0b1bfde2406c05b349831
何炫熠 <24371385@buaa.edu.cn>
2025-10-14 10:51:33 +0800

    always to assign,add modulename

--- P1/alu.v
+++ P1/alu.v
@@ -1,24 +1,15 @@
 `timescale 1ns / 1ps
 module alu(
-    input A[31:0],
-    input B[31:0],
-    input ALUop[2:0],
-    output C[31:0]
-);
-    always @(*) begin
-        if (ALUop == 3'b000) begin
-            C = A + B;
-        end else if (ALUop == 3'b001) begin
-            C = A - B;
-        end else if (ALUop == 3'b010) begin
-            C = A & B;
-        end else if (ALUop == 3'b011) begin
-            C = A | B;
-        end else if (ALUop == 3'b100) begin
-            C = A >> B ;
-        end else if (ALUop == 3'b101) begin
-            C = $signed(A) >>> B;
-        end
-    end
-
+    input [31:0] A,
+    input [31:0] B,
+    input [2:0] ALUop,
+    output [31:0] C
+	);
+    assign C = (ALUop == 3'b000) ? A + B :
+					(ALUop == 3'b001) ? A - B :
+					(ALUop == 3'b010) ? A & B :
+					(ALUop == 3'b011) ? A | B :
+					(ALUop == 3'b100) ? A >> B :
+					(ALUop == 3'b101) ? $signed(A) >>> B :
+					32'd0;
 endmodule
