0387cee95ed9f6508b064a041b0c15c187b6a32a
何炫熠 <24371385@buaa.edu.cn>
2025-10-14 10:52:19 +0800

    ALUop -> ALUOp

--- P1/alu.v
+++ P1/alu.v
@@ -2,14 +2,14 @@
 module alu(
     input [31:0] A,
     input [31:0] B,
-    input [2:0] ALUop,
+    input [2:0] ALUOp,
     output [31:0] C
 	);
-    assign C = (ALUop == 3'b000) ? A + B :
-					(ALUop == 3'b001) ? A - B :
-					(ALUop == 3'b010) ? A & B :
-					(ALUop == 3'b011) ? A | B :
-					(ALUop == 3'b100) ? A >> B :
-					(ALUop == 3'b101) ? $signed(A) >>> B :
+    assign C = (ALUOp == 3'b000) ? A + B :
+					(ALUOp == 3'b001) ? A - B :
+					(ALUOp == 3'b010) ? A & B :
+					(ALUOp == 3'b011) ? A | B :
+					(ALUOp == 3'b100) ? A >> B :
+					(ALUOp == 3'b101) ? $signed(A) >>> B :
 					32'd0;
 endmodule
